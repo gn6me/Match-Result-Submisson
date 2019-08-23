2e58b48b23ac5c4783aff742cbafecdb76f4b4b8
gn6me <mrt.sleeper@gmail.com>
2019-08-23 16:08:44 -0400

    Update submit.php
    Removed specific stuff

--- submit.php
+++ submit.php
@@ -21,15 +21,15 @@
         
         $msg = "$player1 VS $player2 \n \n $player1 \n Shield: $p1_shield \n Ban: $p1_ban \n \n $player2 \n Shield: $p2_shield \n Ban: $p2_ban \n \n Game 1 Result: \n $p1_game1 VS $p2_game1 \n $game1_result \n \n Game 2 Result: \n $p1_game2 VS $p2_game2 \n $game2_result \n \n Game 3 Result: \n $p1_game3 VS $p2_game3 \n $game3_result \n \n Replay Links: \n $replay_link \n \n Screenshots: \n $screenshots";
 
-        $email_from = "test@test.com";
-        $email_subject = "Test form";
+        $email_from = ""; //ADD FROM EMAIL
+        $email_subject = "Aspirant Match Resul";
         $email_body = $msg;
 
-        $to = "thegn6me@gmail.com";
+        $to = ""; //ADD YOUR EMAIL
         $headers = "From: $email_from \r\n";
         $headers .= "Reply-To: $email \r\n";
         mail ($to,$email_subject,$email_body,$headers);
 
-        echo "Thank you"; 
+        echo "Thank you"; //EDIT THANK YOU
 
 ?>
